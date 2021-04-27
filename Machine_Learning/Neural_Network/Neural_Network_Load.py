8bf3dee99974fd6d150d67138fe98d649cc3f659
John Li <lij19@wit.edu>
2021-04-27 14:04:56 -0400

    fixed small parts of file that did not work after file transfer

--- Machine_Learning/Neural_Network/Neural_Network_Load.py
+++ Machine_Learning/Neural_Network/Neural_Network_Load.py
@@ -28,67 +28,31 @@ def HeatMap(real, predicted, le):
     y_unique = np.array(Y)
     y_unique = np.unique(y_unique) #unique Y values
     decoded_predicted =decode(le, predicted)
-    mat = confusion_matrix(y_true=real, y_pred=decoded_predicted)
-    sns.heatmap(mat.T, square = True, annot=True, fmt = "d", xticklabels=y_unique,yticklabels=y_unique,normalize=normalize)
+    mat = confusion_matrix(y_true=real, y_pred=decoded_predicted,normalize="true")
+    sns.heatmap(mat.T, square = True, annot=True, fmt = ".4f", xticklabels=y_unique,yticklabels=y_unique)
     plt.xlabel("true labels")
     plt.ylabel("predicted label")
     plt.title("Predicted Values vs. Real Values")
-    plt.figure(figsize=(10, 16)) #change size
     plt.show()
     #accuracy report based on given confusion matrix
-    print("The accuracy is {}".format(accuracy_score(real, decoded_predicted)))
+    print("The accuracy is {}".format(accuracy_score(real, decoded_predicted))) #prints accuracy
 
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 #read in data using pandas
-train_df = pd.read_csv("Web_Scraping/test.csv")
+train_df = pd.read_csv("Datasets/test2.csv")
 
-model = keras.models.load_model("model2.h5")
+model = keras.models.load_model("model.h5") #loads previously trained model
 model.summary()
+
+#processes dataset
 le = preprocessing.LabelEncoder()
 Y = train_df['publication']
 le.fit(Y)
-string = train_df['content']
-string = np.array(string)
-predictions = model.predict(string, batch_size=32)
-#predictions = decode(le, predictions)
+content = train_df['content']
+content = np.array(content)
+
+#this is where the model begins to predict
+print("Model Predicting")
+predictions = model.predict(content, batch_size=32)
+print("Predictions finished. Creating Heatmap")
 HeatMap(Y, predictions, le)
-print(decode(le, predictions))
-
-
-#everything bellow doesnt matter
-#Get X
-X = list(train_df['content'])
-#Get Y
-Y = list(train_df['publication'])
-
-#Preprocessing by converting string to int
-le = preprocessing.LabelEncoder()
-le.fit(Y)
-
-x_enc = X
-y_enc = encode(le, Y)
-
-x_train, x_test, y_train, y_test = train_test_split(x_enc, y_enc, test_size=0.4, random_state=42)
-#convert everything from list to array
-x_train = np.array(x_train)
-x_test = np.array(x_test)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
-
-predicts = model.predict(x_test, batch_size=32)
-
-y_test = decode(le, y_test)
-y_preds = decode(le, predicts)
-
-#Creation of heatmap
-y_unique = np.array(Y)
-y_unique = np.unique(y_unique) #unique Y values
-mat = confusion_matrix(y_true=y_test, y_pred=y_preds)
-sns.heatmap(mat.T, square = True, annot=True, fmt = "d", xticklabels=y_unique,yticklabels=y_unique,normalize=normalize)
-plt.xlabel("true labels")
-plt.ylabel("predicted label")
-plt.title("Predicted Values vs. Real Values")
-plt.show()
-
-#accuracy report based on given confusion matrix
-print("The accuracy is {}".format(accuracy_score(y_test, y_preds)))
