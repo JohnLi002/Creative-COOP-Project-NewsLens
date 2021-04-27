b2222b48a498df12a1949f32a187a32f5b402fba
John Li <lij19@wit.edu>
2021-04-27 16:48:41 -0400

    changed some stuff by getting rid of unnceccessary parts and altering others so that it works

--- Machine_Learning/Naive_Bayes/Naive_Bayes.py
+++ Machine_Learning/Naive_Bayes/Naive_Bayes.py
@@ -6,7 +6,6 @@
 import numpy as np, pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt #https://github.com/matplotlib/matplotlib
-from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
@@ -16,12 +15,8 @@ from sklearn.model_selection import train_test_split
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 
-from sklearn.model_selection import GridSearchCV
-
-#sns.set() # use seaborn plotting style
-
 # Load the dataset
-data = pd.read_csv('Web_Scraping/test.csv')
+data = pd.read_csv('Datasets/test.csv')
 # Get the text categories
 text_categories = data.info()
 
@@ -44,32 +39,16 @@ data_tf = cv.fit_transform(data.content)
 trainX,testX,trainY,testY = train_test_split(data_tf,data.publication)
 
 #Creation of model
-mnb = MultinomialNB() #class_prior=[.25,.75], fit_prior= True
+mnb = MultinomialNB()
 mnb.fit(trainX,trainY)
 mnb.class_prior
 y_pred = mnb.predict(testX)
-mat = confusion_matrix(y_true=testY, y_pred=y_pred)
-
-#hyperparameter tuning
-#hyper = {'C':[0.05,0.1,0.2,0.3,0.25,0.4,0.5,0.6,0.7,0.8,0.9,1],
-#         'gamma':[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0],
-#         'kernel':['rbf','linear']
-#        }
-
-#gd=GridSearchCV(estimator=svm.SVC(),param_grid=hyper,verbose=True)
-
-#gd.fit(X,Y)
-#print(gd.best_score_)
-#print(gd.best_estimator_)
-
+mat = confusion_matrix(y_true=testY, y_pred=y_pred,normalize='true')
 
 #Creation of heatmap
-sns.heatmap(mat.T, square = True, annot=True, fmt = "d", xticklabels=data.publication.unique(),yticklabels=data.publication.unique())
+sns.heatmap(mat.T, square = True, annot=True, fmt = ".4f", xticklabels=data.publication.unique(),yticklabels=data.publication.unique())
 plt.xlabel("true labels")
 plt.ylabel("predicted label")
 plt.show()
-
-
-
 
 print("The accuracy is {}".format(accuracy_score(testY, y_pred)))
